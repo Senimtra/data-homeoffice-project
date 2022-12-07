b7c604b5b7a02a643cf7244dfcd9cbce4244c705
Senimtra <senimtra@hotmail.com>
2022-12-07 04:52:25 +0100

    add routing

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.home)
+   path('', views.home, name = 'home'),
+   path('analysis', views.analysis, name = 'analysis'),
+   path('employees', views.employees, name = 'employees'),
+   path('companies', views.companies, name = 'companies')
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.template import loader
 def home(request):
    template = loader.get_template('home.html')
    return HttpResponse(template.render())
+
+def analysis(request):
+   template = loader.get_template('analysis.html')
+   return HttpResponse(template.render())
+
+def employees(request):
+   template = loader.get_template('employees.html')
+   return HttpResponse(template.render())
+
+def companies(request):
+   template = loader.get_template('companies.html')
+   return HttpResponse(template.render())
