2e802abd7fbdda1220e111cf20554017405ec449
Senimtra <senimtra@hotmail.com>
2022-12-07 17:06:05 +0100

    create employee survey form

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
    path('', views.home, name = 'home'),
    path('analysis', views.analysis, name = 'analysis'),
    path('employees', views.employees, name = 'employees'),
-   path('companies', views.companies, name = 'companies')
+   path('companies', views.companies, name = 'companies'),
+   path('employees/add', views.add_employee, name = 'add_employee')
 ]
--- core/views.py
+++ core/views.py
@@ -1,18 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Employees
 
 def home(request):
    template = loader.get_template('home.html')
    return HttpResponse(template.render())
 
 def analysis(request):
+   employees = Employees.objects.all()[:10].values()
    template = loader.get_template('analysis.html')
-   return HttpResponse(template.render())
+   context = {'employees': employees}
+   return HttpResponse(template.render(context))
 
 def employees(request):
    template = loader.get_template('employees.html')
-   return HttpResponse(template.render())
+   return HttpResponse(template.render({}, request))
 
 def companies(request):
    template = loader.get_template('companies.html')
    return HttpResponse(template.render())
+
+def add_employee(request):
+   gender = request.POST['gender']
+   age = request.POST['age']
+   children = request.POST['children']
+   rural = request.POST['rural']
+   executive = request.POST['executive']
+   employees = request.POST['employees']
+   wfh_days = request.POST['wfh_days']
+   more_breaks = request.POST['more_breaks']
+   wfh_day_condition = request.POST['wfh_day_condition']
+   productivity = request.POST['productivity']
+   entry = Employees(gender=gender, age=age, children=children, rural=rural, executive=executive, employees=employees, wfh_days=wfh_days, more_breaks=more_breaks, wfh_day_condition=wfh_day_condition, productivity=productivity)
+   entry.save()
+   return HttpResponseRedirect(reverse('home'))
