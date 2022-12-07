b8b23624074f22b623fe2ee82ca8d89e85eaa118
Senimtra <senimtra@hotmail.com>
2022-12-07 06:55:38 +0100

    create employee model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Employees(models.Model):
+   gender = models.CharField(max_length = 23)
+   age = models.CharField(max_length = 5)
+   children = models.BooleanField()
+   rural = models.BooleanField()
+   executive = models.BooleanField()
+   employees = models.CharField(max_length = 6)
+   wfh_days = models.IntegerField()
+   more_breaks = models.BooleanField()
+   wfh_day_condition = models.CharField(max_length = 23)
+   productivity = models.CharField(max_length = 25)
