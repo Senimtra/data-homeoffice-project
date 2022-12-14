89f7e114147c46e35bd9dfa4916d5e57b58c3d30
Senimtra <senimtra@hotmail.com>
2022-12-14 17:01:38 +0100

    add executive dimension to amount rem. days chart

--- charts/data.py
+++ charts/data.py
@@ -84,6 +84,7 @@ def remote_days():
    remote_days = []; data = []
    rural_days = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
    children_days = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+   executive_days = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}  
    # Remote days total
    days_query = Employees.objects.values('wfh_days').annotate(count = Count('wfh_days'))
    [remote_days.append((days['wfh_days'], days['count'])) for days in days_query]
@@ -96,6 +97,10 @@ def remote_days():
    rural_query = Employees.objects.filter(rural = 'Yes').values('wfh_days').annotate(count = Count('wfh_days'))
    for entry in rural_query:
       rural_days[entry['wfh_days']] = entry['count']
+   # Remote days executive
+   executive_query = Employees.objects.filter(executive = 'Yes').values('wfh_days').annotate(count = Count('wfh_days'))
+   for entry in executive_query:
+      executive_days[entry['wfh_days']] = entry['count']
    # Set up context
-   context = {'remote_days_data': json.dumps(data), 'remote_days_rural': json.dumps(list(rural_days.values())), 'remote_days_children': json.dumps(list(children_days.values()))}
+   context = {'remote_days_data': json.dumps(data), 'remote_days_rural': json.dumps(list(rural_days.values())), 'remote_days_children': json.dumps(list(children_days.values())), 'remote_days_executive': json.dumps(list(executive_days.values()))}
    return context
